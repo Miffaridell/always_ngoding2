6f2bf18ad04ded70d1c29ed5c61d64365c40f0ba
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-10 21:32:39 +0700

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class day2 {
+
+    public static void main(String[] args) {
+        String firstName = "MIFTAH";
+        String lastName = "FARIDL.S";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
